af263e61e40bbee57444ba4fc029757f7e40a0c2
xattle <ntgrgr@gmail.com>
2021-07-27 16:40:22 -0500

    Init commit

---
+++ main.js
@@ -0,0 +1,55 @@
+// main.js
+
+// Modules to control application life and create native browser window
+const { app, BrowserWindow, globalShortcut } = require('electron')
+const path = require('path')
+
+function createWindow () {
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js')
+    }
+  })
+
+  // and load the index.html of the app.
+  mainWindow.loadFile('index.html')
+
+  // Open the DevTools.
+  // mainWindow.webContents.openDevTools()
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.whenReady().then(() => {
+  var robot = require("robotjs");
+
+  createWindow()
+
+  app.on('activate', function () {
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+
+  // Register Global shortcuts
+  globalShortcut.register('CommandOrControl+Shift+F1', () => {
+    console.log('Global recognized, sending data.')
+    robot.setKeyboardDelay(0);
+    robot.keyToggle('control','up');
+    robot.keyToggle('shift','up');
+    robot.keyToggle('f1','up');
+    robot.typeString("abc123");
+  })
+
+})
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on('window-all-closed', function () {
+  if (process.platform !== 'darwin') app.quit()
+})
